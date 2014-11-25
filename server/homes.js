5c7d0e9584531557fcc51d1e6106cbf9ab3a0224
Andy Jiang <andyjiang@gmail.com>
2014-11-24 22:10:40 -0800

    added reverse geocoder

--- server/homes.js
+++ server/homes.js
@@ -3,13 +3,28 @@
  * Module dependencies.
  */
 
+var geocoder = require('geocoder');
+var thunkify = require('thunkify');
 var Zillow = require('../lib/zillow');
+var reverseGeocode = thunkify(geocode.reverseGeocode);
 
 /**
  * Define `Homes`.
  */
 
 var Homes = {};
+
+/**
+ * Get link with homes near the user.
+ *
+ * @param {Object} user
+ */
+
+Homes.get = function *get(user) {
+  var address = yield reverseGeocode(user.lat, user.lng);
+  console.log(address);
+};
+
 
 /**
  * Expose `Homes`.
