af861a9c860870437a9edfb9de62cbceb4894219
Andy Jiang <andyjiang@gmail.com>
2014-12-04 20:36:24 -0800

    refactored a bit, wired it up

--- server/homes.js
+++ server/homes.js
@@ -3,6 +3,7 @@
  * Module dependencies.
  */
 
+var Kimono = require('../lib/kimono');
 var Zillow = require('../lib/zillow');
 var Geocoder = require('geocoder');
 var thunkify = require('thunkify');
@@ -21,18 +22,23 @@ var Homes = {};
  */
 
 Homes.get = function *get(user) {
-  var geos = yield geo.reverseGeocode(user.lat, user.lng);
-  var address = geos.results[0].formatted_address;
-  var array = address.split(',');
-  var load = {
-    address: array[0].trim(),
-    city: array[1].trim(),
-    state: array[2].split(' ')[1],
-    zip: array[2].split(' ')[2],
-    rentestimate: true
-  };
-  var zillow = yield Zillow.getDeepSearchResults(load);
+  // var geos = yield geo.reverseGeocode(user.lat, user.lng);
+  // var address = geos.results[0].formatted_address;
+  // var array = address.split(',');
+  // var load = {
+  //   address: array[0].trim(),
+  //   city: array[1].trim(),
+  //   state: array[2].split(' ')[1],
+  //   zip: array[2].split(' ')[2],
+  //   rentestimate: true
+  // };
+  // var zillow = yield Zillow.getDeepSearchResults(load);
   // What if no result? How to get nearby address?
+
+  // Alternative.
+  var data = yield Kimono.get(lat, lng);
+  // Normalize data.
+  // Build query string.
   return link;
 };
 
@@ -41,3 +47,32 @@ Homes.get = function *get(user) {
  */
 
 module.exports = Homes;
+
+/**
+ * Private function to normalize home prices.
+ *
+ * TODO: generalize this function, too tightly coupled with data format from Kimono
+ *
+ * @param {Array} homes
+ * @return {Integer}
+ */
+
+function normalizer(homes) {
+  var totalBedrooms = 0;
+  var totalPrice = 0;
+  homes.forEach(function(home) {
+    if (home.apt_title.text.slice(1, 3) === 'bd' && home.price.length < 8) {
+      totalBedrooms += parseInt(home.apt_title.text.slice(0, 1));
+      totalPrice += parseInt(home.price.replace('$', '').replace(',', ''));
+    }
+  });
+  return totalPrice.length / totalBedrooms.length;
+}
+
+/**
+ * Private function to build querystring.
+ *
+ */
+
+function buildQueryString() {
+}
